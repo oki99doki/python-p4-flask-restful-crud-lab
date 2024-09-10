ea470f071a69cef91654e0656026ced96959aba9
Stefan Klinkert <s.klinkert@gmail.com>
2024-09-09 21:09:12 -0400

    complete REST CRUD Lab

--- server/app.py
+++ server/app.py
@@ -46,9 +46,27 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        data = request.get_json()
+        for cur_field in data:
+            setattr(plant, cur_field, data.get(cur_field))
+        db.session.add(plant)
+        db.session.commit()
+        return make_response(plant.to_dict(),200)
+    
+    def delete(self, id):
+         plant = Plant.query.filter_by(id=id).first()
+         db.session.delete(plant)
+         db.session.commit()
+         return make_response({}, 204)
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
+
+
+
 
 
 if __name__ == '__main__':
